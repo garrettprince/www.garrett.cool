f93f3afd0204a5bb747bceb39aa0ae739bd98b9e
garrettprince <garrettjamesprince@gmail.com>
2023-09-19 15:17:09 +0200

    Pixel test to see performance

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,6 +1,13 @@
 // Package imports
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
+  Bloom,
+  DepthOfField,
+  EffectComposer,
+  Pixelation,
+  Vignette,
+} from "@react-three/postprocessing";
+import {
   Environment,
   Float,
   OrbitControls,
@@ -39,91 +46,95 @@ export default function Scene({ action, setAction }) {
     //   zoom: 100,
     // }}
     >
-      {/* <OrbitControls /> */}
+      <EffectComposer>
+        <Pixelation granularity={5} />
+        <Bloom />
+        {/* <OrbitControls /> */}
 
-      {/* Lighting */}
-      <ambientLight intensity={1} />
-      <directionalLight position={[4, 5, 6]} intensity={1} color={"#fff"} />
-      <directionalLight position={[1, 1, 1]} intensity={1} color={"#fff"} />
+        {/* Lighting */}
+        <ambientLight intensity={1} />
+        <directionalLight position={[4, 5, 6]} intensity={1} color={"#fff"} />
+        <directionalLight position={[1, 1, 1]} intensity={1} color={"#fff"} />
 
-      <Environment preset="warehouse" />
+        <Environment preset="night" />
 
-      {/*  maybe add subtle sparkles later, read drei docs */}
-      {/* <Sparkles /> */}
+        {/*  maybe add subtle sparkles later, read drei docs */}
+        {/* <Sparkles /> */}
 
-      {/* Background Sphere */}
-      {/* <Background action={action} setAction={setAction} /> */}
+        {/* Background Sphere */}
+        {/* <Background action={action} setAction={setAction} /> */}
 
-      {/* Objects */}
-      <Float
-        action={action}
-        speed={1} // Animation speed, defaults to 1
-        rotationIntensity={action === "home" ? 0.7 : 0.25} // XYZ rotation intensity, defaults to 1
-        floatIntensity={0.6} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-        floatingRange={action === "home" ? [0.1, -0.1] : [0.05, 0.05]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
-      >
-        {/* LOOK INTO PRESENTATION CONTROLS MORE */}
-        {/* <PresentationControls
+        {/* Objects */}
+        <Float
+          action={action}
+          speed={1} // Animation speed, defaults to 1
+          rotationIntensity={action === "home" ? 0.7 : 0.25} // XYZ rotation intensity, defaults to 1
+          floatIntensity={0.6} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+          floatingRange={action === "home" ? [0.1, -0.1] : [0.05, 0.05]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+        >
+          {/* LOOK INTO PRESENTATION CONTROLS MORE */}
+          {/* <PresentationControls
           action={action}
           enabled={action === "home" ? true : false}
         > */}
-        <GarrettFace action={action} setAction={setAction} />
-        {/* </PresentationControls> */}
-      </Float>
-      <Float
-        action={action}
-        speed={1}
-        rotationIntensity={0.6}
-        floatIntensity={0.7}
-        floatingRange={[-0.04, 0.04]}
-      >
-        <NameTag action={action} setAction={setAction} />
-      </Float>
-      <Float
-        action={action}
-        speed={1}
-        rotationIntensity={0.6}
-        floatIntensity={0.7}
-        floatingRange={[-0.04, 0.04]}
-      >
-        <PlayDate action={action} setAction={setAction} />
-      </Float>
-      <Float
-        action={action}
-        speed={1}
-        rotationIntensity={0.6}
-        floatIntensity={0.7}
-        floatingRange={[-0.04, 0.04]}
-      >
-        <ContactPhone action={action} setAction={setAction} />
-      </Float>
-      <Float
-        action={action}
-        speed={1}
-        rotationIntensity={0.6}
-        floatIntensity={0.7}
-        floatingRange={[-0.04, 0.04]}
-      >
-        <Portfolio action={action} setAction={setAction} />
-      </Float>
-      <Float
-        action={action}
-        speed={1}
-        rotationIntensity={0.6}
-        floatIntensity={0.7}
-        floatingRange={[-0.04, 0.04]}
-      >
-        <Montanahead action={action} setAction={setAction} />
-      </Float>
-      <Float
-        action={action}
-        speed={1}
-        rotationIntensity={0.6}
-        floatIntensity={0.7}
-        floatingRange={[-0.04, 0.04]}
-      >
-        <LongAddition action={action} setAction={setAction} />
-      </Float>
+          <GarrettFace action={action} setAction={setAction} />
+          {/* </PresentationControls> */}
+        </Float>
+        <Float
+          action={action}
+          speed={1}
+          rotationIntensity={0.6}
+          floatIntensity={0.7}
+          floatingRange={[-0.04, 0.04]}
+        >
+          <NameTag action={action} setAction={setAction} />
+        </Float>
+        <Float
+          action={action}
+          speed={1}
+          rotationIntensity={0.6}
+          floatIntensity={0.7}
+          floatingRange={[-0.04, 0.04]}
+        >
+          <PlayDate action={action} setAction={setAction} />
+        </Float>
+        <Float
+          action={action}
+          speed={1}
+          rotationIntensity={0.6}
+          floatIntensity={0.7}
+          floatingRange={[-0.04, 0.04]}
+        >
+          <ContactPhone action={action} setAction={setAction} />
+        </Float>
+        <Float
+          action={action}
+          speed={1}
+          rotationIntensity={0.6}
+          floatIntensity={0.7}
+          floatingRange={[-0.04, 0.04]}
+        >
+          <Portfolio action={action} setAction={setAction} />
+        </Float>
+        <Float
+          action={action}
+          speed={1}
+          rotationIntensity={0.6}
+          floatIntensity={0.7}
+          floatingRange={[-0.04, 0.04]}
+        >
+          <Montanahead action={action} setAction={setAction} />
+        </Float>
+        <Float
+          action={action}
+          speed={1}
+          rotationIntensity={0.6}
+          floatIntensity={0.7}
+          floatingRange={[-0.04, 0.04]}
+        >
+          <LongAddition action={action} setAction={setAction} />
+        </Float>
+      </EffectComposer>
     </Canvas>
   );
 }
