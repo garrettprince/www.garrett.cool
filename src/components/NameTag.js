d3bf7fcbd1bb018d690136bfb41effe681dd67b5
garrettprince <garrettjamesprince@gmail.com>
2023-10-11 17:37:17 +0700

    small license fix

--- src/components/NameTag.js
+++ src/components/NameTag.js
@@ -90,3 +90,72 @@ export default function NameTag({ action, setAction, ...props }) {
     </animated.group>
   );
 }
+
+
+
+
+
+// export default function Scene({ ...props }) {
+//   const { nodes, materials } = useSpline('https://prod.spline.design/KnnuFsmNwhd4ZZAV/scene.splinecode')
+//   return (
+    
+//       <animated.group
+//       ref={meshRef}
+//       scale={scale}
+//       position={position}
+//       onClick={() =>
+//         action === "home" ? setAction("nameTag") : setAction("home")
+//       }
+//     >
+//           <group name="Group 3" position={[129.64, 107.43, -0.25]}>
+//             <mesh
+//               name="newlicense2.png"
+//               geometry={nodes['newlicense2.png'].geometry}
+//               material={materials['newlicense2.png Material']}
+//               castShadow
+//               receiveShadow
+//               position={[0.16, 1.36, 3.85]}
+//             />
+//             <mesh
+//               name="backlicense2.png"
+//               geometry={nodes['backlicense2.png'].geometry}
+//               material={materials['backlicense2.png Material']}
+//               castShadow
+//               receiveShadow
+//               position={[-0.12, -0.58, -3.75]}
+//               rotation={[-3.14, 0, 3.12]}
+//               scale={1}
+//             />
+//             <group name="card" position={[0, 0.02, 2.04]} scale={[0.91, 0.86, 0.91]}>
+//               <mesh
+//                 name="Rectangle 2"
+//                 geometry={nodes['Rectangle 2'].geometry}
+//                 material={materials['Mat 2']}
+//                 castShadow
+//                 receiveShadow
+//                 position={[0, 0, -6.07]}
+//               />
+//             </group>
+//           </group>
+//           <directionalLight
+//             name="Directional Light"
+//             castShadow
+//             intensity={0.7}
+//             shadow-mapSize-width={1024}
+//             shadow-mapSize-height={1024}
+//             shadow-camera-near={-10000}
+//             shadow-camera-far={100000}
+//             shadow-camera-left={-1250}
+//             shadow-camera-right={1250}
+//             shadow-camera-top={1250}
+//             shadow-camera-bottom={-1250}
+//             position={[200, 300, 300]}
+//           />
+          
+//           <hemisphereLight name="Default Ambient Light" intensity={0.75} color="#eaeaea" />
+//       </animated.group>
+    
+//   )
+// }
+
+
